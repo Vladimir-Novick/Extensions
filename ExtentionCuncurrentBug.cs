9bcff294f15c31cdc697f951fb10aceae08b4789
Vladimir Novick <vlad.novick@gmail.com>
2017-10-19 00:12:40 -0400

    AddRange to CuncurrentBug class

---
+++ ExtentionCuncurrentBug.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Extentions
+{
+    public static class ExtentionCuncurrentBug
+    {
+        public static void AddRange<T>(this ConcurrentBag<T> @this, IEnumerable<T> toAdd)
+        {
+             toAdd.AsParallel().ForAll(t => @this.Add(t));
+        }
+    }
+}
