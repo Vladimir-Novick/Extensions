338b560c22e48ab25f7b4ba40a5019bff927fed8
Vladimir Novick <vlad.novick@gmail.com>
2017-10-22 12:06:54 -0400

    Duplicate object properties in another object

---
+++ ExtentionCopyTo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace Extentions
+{
+    public static class ExtentionCopyTo
+    {
+        public static void CopyTo<T>(this T source, T distanation)
+        {
+            var properties = typeof(T).GetProperties();
+            if (distanation != null)
+            {
+               
+                foreach (var item in properties)
+                {
+                    Object data = item.GetValue(source);
+                    item.SetValue(distanation, data);
+                }
+            }
+
+        }
+
+    }
+}
